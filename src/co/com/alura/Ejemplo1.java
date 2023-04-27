b9f89e8e52b0b865377285b5f2c2ba7a65c321ad
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-27 00:22:45 -0500

    commit 01: creando proyecto Dominando Collections

---
+++ src/co/com/alura/Ejemplo1.java
@@ -0,0 +1,10 @@
+package co.com.alura;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a el curso de Java: Dominando Colletions !!!");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author danieltm1987
+ *
+ */
+module JavaColletionsAlura {
+}
