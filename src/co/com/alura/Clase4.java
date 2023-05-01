84a565f87a321c2c01a639c7f9c51d5d909f6a5f
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-04-30 21:59:15 -0500

    commit 05: ArrayList de objetos y sobre escritura de toString.

---
+++ src/co/com/alura/Clase4.java
@@ -0,0 +1,36 @@
+package co.com.alura;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Clase4 {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a el curso de Java: Dominando Colletions !!!");
+		
+		System.out.println("Clase 4:Trabajando con ArrayList de Objetos !!!\n");
+		
+		Curso curso1 = new Curso("Geometria", 30);
+		Curso curso2 = new Curso("Fisica", 10);
+		Curso curso3 = new Curso("Quimica", 20);
+		Curso curso4 = new Curso("Historia", 50);
+		;
+		
+		
+		ArrayList<Curso> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
+		cursos.add(curso4);
+		
+		System.out.println("Clase 4:Imprimiedo el ArrayList de Objetos !!!\n");
+		System.out.println(cursos);
+		
+		
+
+	}
+
+}
