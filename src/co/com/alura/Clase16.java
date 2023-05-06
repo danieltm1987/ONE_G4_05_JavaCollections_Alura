dea6df068b74d4f05eada5f7b05f70beb4762bca
Daniel Torres Martinez <daniel.tm1987@gmail.com>
2023-05-05 22:12:51 -0500

    commit 16: uso de Set's e Iterators

---
+++ src/co/com/alura/Clase16.java
@@ -0,0 +1,55 @@
+package co.com.alura;
+
+import java.util.Iterator;
+
+import co.com.alura.model.*;
+
+public class Clase16 {
+
+	public static void main(String[] args) {
+		System.out.println("Bienvenido a el curso de Java: Dominando Colletions !!!");
+		
+		System.out.println("Clase 16:Trabajando con Sets e Iterators !!!\n");
+		
+		Curso curso1 = new Curso("Historia", 30);
+		
+		Alumno alumno1 = new Alumno("Daniel Alfonso","001");
+		Alumno alumno2 = new Alumno("Luis Miguel","002");
+		Alumno alumno3 = new Alumno("Jose","003");
+		Alumno alumno4 = new Alumno("Maria Jose","004");
+		Alumno alumno5 = new Alumno("Maria Alejandra","005");
+		Alumno alumno6 = new Alumno("Mafe Roca","006");
+		Alumno alumno7 = new Alumno("Michelle","007");
+		Alumno alumno8 = new Alumno("Mafe Roca","008");
+		Alumno alumno9 = new Alumno("Michelle","010");
+		Alumno alumno10 = new Alumno("Gustavo Polar","052");
+		
+		
+		curso1.addAlumno(alumno1);
+		curso1.addAlumno(alumno2);
+		curso1.addAlumno(alumno3);
+		curso1.addAlumno(alumno4);
+		curso1.addAlumno(alumno5);
+		curso1.addAlumno(alumno6);
+		curso1.addAlumno(alumno7);
+		curso1.addAlumno(alumno8);
+		curso1.addAlumno(alumno9);
+		curso1.addAlumno(alumno10);
+		
+		System.out.println("Clase 16:Imprimiendo Listado de Alumnos!!!\n");
+		curso1.getAlumnos().forEach(alumno -> System.out.println("Alumno :"+alumno));
+		
+		System.out.println("\nClase 16:Usando Iteraros!!!\n");
+		
+		Iterator<Alumno> alumnoIterator = curso1.getAlumnos().iterator();
+		
+		while(alumnoIterator.hasNext()) {
+			System.out.println(alumnoIterator.next());
+		}
+		
+			
+		System.out.println("\nClase 16:Error ya que el iterator fue recorrido en su Totalidad!!!\n");
+		alumnoIterator.next();
+
+	}
+}
